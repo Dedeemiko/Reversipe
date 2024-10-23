eeedc640acf68945c143821d734e9964af7424e1
Dede Emiko <dede.emiko@gmail.com>
2024-10-23 11:13:55 -0400

    Create Search by ingredients and search by recipe pages

---
+++ src/pages/SearchByIngredientsPage/SearchByIngredientsPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchByIngredientsPage() {
+    return (
+        <div>
+            <h2>Search by Ingredients</h2>
+        </div>
+    )
+};
+
+export default SearchByIngredientsPage;
---
+++ src/pages/SearchByRecipePage/SearchByRecipePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchByRecipePage() {
+    return (
+        <div>
+            <h2>Search by Recipe</h2>
+        </div>
+    )
+};
+
+export default SearchByRecipePage;
