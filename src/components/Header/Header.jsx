2adcdad0fd78cb701f628464fcaec34d4388ed0e
Dede Emiko <dede.emiko@gmail.com>
2024-10-23 09:35:47 -0400

    Import partials into header component and style

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import "./Header.scss";
+
+function Header() {
+
+    return (
+        <header className="header">
+            <nav className="header__nav">
+                <div className="header__logo">
+
+                    <h1>Reversipe</h1>
+                    {/* <Link to="/">
+                            <img
+                                src={logo}
+                                alt="Reversipe Logo"
+                                className="header__logo-img"
+                            />
+                    </Link> */}
+
+                </div>
+
+            </nav>
+        
+        </header>
+    );
+    
+
+
+
+
+}
+
+export default Header;
