759d59fe4da786b5232905750896512b2910b52c
Dede Emiko <dede.emiko@gmail.com>
2024-10-23 23:28:45 -0400

    Add routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./App.scss";
 import Header from "./components/Header/Header";
-
-
+import Footer from "./components/Footer/Footer";
+import Homepage from "./pages/HomePage/HomePage";
+import SearchByIngredientsPage from "./pages/SearchByIngredientsPage/SearchByIngredientsPage";
+import SearchByRecipePage from "./pages/SearchByRecipePage/SearchByRecipePage";
 
 
 function App() {
   
 
   return (
-    <>
-      <Header />
-    </>
+    <BrowserRouter>
+        <div className="App">
+            <Header />
+            <main>
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/search-by-ingredients" element={<SearchByIngredientsPage />} />
+                  <Route path="/search-by-recipe" element={<SearchByRecipePage />} />
+                </Routes>
+            </main>
+          
+            <Footer />
+        </div>
+      
+    </BrowserRouter>
   )
 }
 
