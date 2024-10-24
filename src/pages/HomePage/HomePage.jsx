5cad51f2282302ab9b1d30428dd74ab006a2288a
Dede Emiko <dede.emiko@gmail.com>
2024-10-23 23:29:09 -0400

    Create Home page page and scss

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HomePageContent from "../../components/HomePageContent/HomePageContent";
+
+
+function Homepage() {
+    return (
+        <div className="home-page">
+            <HomePageContent />
+        </div>
+    );
+}
+
+export default Homepage;
