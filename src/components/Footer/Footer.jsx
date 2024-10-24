e2889129b431db7f61d957e67c6ca51ed84493b9
Dede Emiko <dede.emiko@gmail.com>
2024-10-23 23:53:58 -0400

    Add content to footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import "./Footer.scss";
+
+function Footer() {
+
+    return (
+        <footer className="footer">
+            <h3 className="footer__header">
+                Contact Us
+            </h3>
+
+            <p>email</p>
+        </footer>
+        );
+    }
+
+
+export default Footer;
