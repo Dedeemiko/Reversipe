e2b2304f85eb928e09e3a864e3cc91f5482f1e62
Dede Emiko <dede.emiko@gmail.com>
2024-10-24 15:11:27 -0400

    Import search input in search by recipe page

--- src/pages/SearchByRecipePage/SearchByRecipePage.jsx
+++ src/pages/SearchByRecipePage/SearchByRecipePage.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import SearchInput from "../../components/SearchInput/SearchInput";
+
 
 function SearchByRecipePage() {
+    const [searchItem, setSearchItem] = useState("");
+
+    const handleSearch = (newSearchItem) => {
+        setSearchItem(newSearchItem);
+        
+    };
+
     return (
-        <div>
+        <div className="Search-ingredients">
             <h2>Search by Recipe</h2>
+
+            <SearchInput
+                value={searchItem}
+                onChange={handleSearch}
+                placeholder="Search Recipe..."
+            />
         </div>
     )
 };
