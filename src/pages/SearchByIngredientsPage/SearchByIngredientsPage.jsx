e57355e2498d6547ceb012b2e5077d765f132754
Dede Emiko <dede.emiko@gmail.com>
2024-10-25 08:33:38 -0400

    Update search by ingredient and recipe pages to include backend

--- src/pages/SearchByIngredientsPage/SearchByIngredientsPage.jsx
+++ src/pages/SearchByIngredientsPage/SearchByIngredientsPage.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 import SearchInput from "../../components/SearchInput/SearchInput";
 import Button from "../../components/Button/Button";
+import { baseUrl } from "../../utils/api";
 import "./SearchByIngredientsPage.scss";
 
 
 function SearchByIngredientsPage() {
-    const [searchItem, setSearchItem] = useState("");
+    const [ingredients, setIngredients] = useState("");
+    const [recipes, setRecipes] = useState([]);
 
-    const handleSearch = (newSearchItem) => {
-        setSearchItem(newSearchItem);
-        
+    const handleSearch = async () => {
+        try {
+            const response = await axios.get(`${baseUrl}api/recipes/ingredients-search`, { params: { ingredients: ingredients },
+            });
+            setRecipes(response.data);
+        } catch (error) {
+            console.error("Error searching for recipes", error);
+        }
     };
 
     return (
@@ -18,8 +26,8 @@ function SearchByIngredientsPage() {
 
             <div className="search-ingredients__options">
                 <SearchInput
-                    value={searchItem}
-                    onChange={handleSearch}
+                    value={ingredients}
+                    onChange={setIngredients}
                     placeholder="Enter ingredients..."
                 />
 
@@ -28,6 +36,26 @@ function SearchByIngredientsPage() {
                     text="Search"
                     handleClick={handleSearch}
                 />
+            </div>
+
+            <div className="search-ingredients__results">
+                {recipes.length > 0 ? (
+                    <ul>
+                        {recipes.map((recipe) => (
+                            <li key={recipe.id}>
+                                <img src={recipe.picture_link} alt={recipe.title} />
+                                <h3>{recipe.title}</h3>
+                                <p>Estimated Time: {recipe.estimated_time} minutes</p>
+                                <p>Ingredients Needed: {recipe.matchRate}
+                                </p>
+                            </li>
+                        )
+                        )}
+                    </ul>
+                ) : (
+                    <p>No recipes found</p>
+                )}
+
             </div>
         </div>
     )
--- src/pages/SearchByRecipePage/SearchByRecipePage.jsx
+++ src/pages/SearchByRecipePage/SearchByRecipePage.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 import SearchInput from "../../components/SearchInput/SearchInput";
 import Button from "../../components/Button/Button";
+import { baseUrl } from "../../utils/api";
 import "./SearchByRecipePage.scss";
 
 
 function SearchByRecipePage() {
     const [searchItem, setSearchItem] = useState("");
+    const [recipes, setRecipes] = useState([]);
 
-    const handleSearch = (newSearchItem) => {
-        setSearchItem(newSearchItem);
+    const handleSearch = async () => {
+        try {
+            const response = await axios.get(`${baseUrl}api/recipes/search`, { params: { name: searchItem },
+            });
+            setRecipes(response.data);
+        } catch (error) {
+            console.error("Error searching for recipes", error);
+        }
         
     };
 
@@ -19,7 +28,7 @@ function SearchByRecipePage() {
             <div className="search-recipe__options">
                 <SearchInput
                     value={searchItem}
-                    onChange={handleSearch}
+                    onChange={setSearchItem}
                     placeholder="Search Recipe..."
                 />
 
@@ -28,6 +37,26 @@ function SearchByRecipePage() {
                     text="Search"
                     handleClick={handleSearch}
                 />
+            </div>
+
+            <div className="search-recipe__results">
+                {recipes.length > 0 ? (
+                    <ul>
+                        {recipes.map((recipe) => (
+                            <li key={recipe.id}>
+                                <img src={recipe.picture_link} alt={recipe.title} />
+                                <h3>{recipe.title}</h3>
+                                <p>Estimated Time: {recipe.estimated_time} minutes</p>
+                            </li>
+                        )
+                        )}
+                    </ul>
+                ) : (
+
+                    <p>No recipes found</p>
+                )}
+
+
             </div>
         </div>
     )
