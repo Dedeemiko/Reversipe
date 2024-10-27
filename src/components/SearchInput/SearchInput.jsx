f5532f28150c71e2fb45cef18de09eef8d3a4f17
Dede Emiko <dede.emiko@gmail.com>
2024-10-27 17:49:16 -0400

    Add conditional error message and stlying to show red border around the search input

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import "./SearchInput.scss"
 
-function SearchInput({ value, onChange, placeholder }) {
+
+function SearchInput({ value, onChange, placeholder, errorMessage }) { 
+    
     return (
-        <input
-            type="text"
-            className="search-input"
-            value={value}
-            onChange={(event) => onChange(event.target.value)}
-            placeholder={placeholder}
-        />
+        <div className="search-input__container">
+            <input
+                type="text"
+                className={`search-input ${errorMessage ? "search-input--error" : ""}`}
+                value={value}
+                onChange={(event) => {
+                    onChange(event.target.value);
+                }}
+                placeholder={placeholder}
+            />
+            {errorMessage && <p className="search-input__error">{errorMessage}</p>}
+        </div>
     );
 }
 
