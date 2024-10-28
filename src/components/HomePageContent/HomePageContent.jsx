8e9603c18f573718f2ea0f18ae1e91f3ea3cd54d
Dede Emiko <dede.emiko@gmail.com>
2024-10-27 23:10:29 -0400

    Import useState in homepage content and add loading state and loading icon

--- src/components/HomePageContent/HomePageContent.jsx
+++ src/components/HomePageContent/HomePageContent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HomePageContent.scss";
 import Card from "../Card/Card";
 import { useNavigate } from "react-router-dom";
@@ -8,15 +8,28 @@ import ingredientImage from "../../assets/images/ingredients-img.jpg"
 
 function HomePageContent() {
 
+    const [loading, setLoading] = useState(false);
     const userName = "Dede";
     const navigate = useNavigate();
 
+    const handleNavigate = (path) => {
+        setLoading(true);
+        navigate(path);
+    };
+
     return (
         <div className="homepage-content">
-            <h2>Welcome {userName}</h2>
+            <h2>Hello {userName}</h2>
             <p>What would you like to do today?</p>
 
-            <div className="homepage-content__options">
+            {loading && (
+                <div className="loading-spinner">
+                    <div className="spinner"></div>
+                    <p>Loading...</p>
+                </div>
+            )}
+
+            <div className={`homepage-content__options ${loading ? "loading" : ''}`}>
                 <Card
                     title="Search by Ingredients"
                     imageSrc={ingredientImage}
